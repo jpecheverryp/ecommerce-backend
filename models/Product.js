369083e510eedc552d217b79522f462364131a11
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-03-17 03:32:47 -0400

    fix an error with the associations of categories and products by changing the name of the IDs to be more specific

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,7 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id: {
+    product_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
@@ -38,7 +38,7 @@ Product.init(
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
-        key: 'id'
+        key: 'category_id'
       }
     }
   },
