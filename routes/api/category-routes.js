fb9b9d268064c8f5b10f7d845a2b00bf51a44b7c
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-03-18 18:18:21 -0400

    added category routes for updating, creating and getting an specific category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,21 +12,43 @@ router.get('/', async (req, res) => {
   } catch (error) {
     res.status(500).json(error)
   }
-  // find all categories
-  // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    
+    if (!categoryData) {
+      res.status(404).json({message: 'Category Not Found!'});
+      return
+    }
+
+    res.status(200).json(categoryData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.findByPk(req.params.id)
+    updatedCategory.category_name = req.body.category_name;
+    await updatedCategory.save();
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    res.status(400).json(error)
+  }
 });
 
 router.delete('/:id', (req, res) => {
